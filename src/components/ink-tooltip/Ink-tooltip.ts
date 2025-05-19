5b31feb23c80a14be309a5f86deabaed1a003f6b
TNL-JoyceWen <joyce.wen@thenewslens.com>
2025-05-19 11:45:59 +0800

    [sc-79980] style: add style

--- src/components/ink-tooltip/Ink-tooltip.ts
+++ src/components/ink-tooltip/Ink-tooltip.ts
@@ -1,4 +1,5 @@
 import 'floating-vue/dist/style.css'
+import '@/scss/component/_ink-vtooltip.scss'
 import InkVTooltip from '@/components/ink-tooltip/InkVTooltip.vue'
 import { vTooltip as vInkTooltip } from 'floating-vue'
 export { vInkTooltip, InkVTooltip }
